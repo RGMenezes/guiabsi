9945b6ee5b08fde73bd8816521e6ca344839ce29
Italo Hespanhol Peres <pereshespanhol1@gmail.com>
2026-07-29 23:50:57 -0300

    basic interface of videos page

--- documentation/horas.md
+++ documentation/horas.md
@@ -2,3 +2,6 @@
 
 *nome:* Gabriel Ribeiro Ciafrino.
 *Horas dedicadas:* 37h:37min.
+
+*nome:* Ítalo Hespanhol Peres.
+*Horas dedicadas:* 2H:45min.
--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,4 +1,28 @@
+/*
+Componente de pagina padrão.
+import TopBar from "@/components/TopBar";
+import BackButton from "@/components/BackButton";
+import Header from "@/components/Header";
+import Category from "@/components/Category";
+*/
+
+import BackButton from "@/components/BackButton";
+import Header from "@/components/Header";
+import TopBar from "@/components/TopBar";
+
 export default function VideosPage() { 
-  return (<h1>Ola <h2>mundo</h2></h1>
+
+  return (
+    <>
+      <TopBar/>
+      <main>
+        <BackButton/>
+        <Header titleText="Vídeos de localização">
+          Encontre vídeos curtos mostrando como chegar aos principais locais do campus, facilitando sua locomoção nos primeiros dias.
+        </Header>
+          
+      </main>
+    </>
   );
 }
+
